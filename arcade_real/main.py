021ab3d012ce25b1c1b7fe42fe5c02e9a7655ce0
T.Prakasit <prakasit.tar@mail.kmutt.ac.th>
2021-10-24 16:12:44 +0700

    modify 24/10/2021

--- arcade_real/main.py
+++ arcade_real/main.py
@@ -1,18 +1,23 @@
 import arcade
 import os
 import json
+import math
 from arcade.scene import Scene
 
+
+#Set The Screen
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 SCREEN_TITLES = "Platformer"
 
+#Set size of the Character
 TILE_SCALING = 0.5
 CHARACTER_SCALING = TILE_SCALING * 1.5
 COIN_SCALING = 0.5
 SPRITE_PIXEL_SIZE = 128
 GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SCALING
 
+#Set movement character speed
 PLAYER_MOVEMENT_SPEED = 5
 GRAVITY = 1
 PLAYER_JUMP_SPEED = 20
@@ -20,6 +25,7 @@ PLAYER_JUMP_SPEED = 20
 PLAYER_X = SPRITE_PIXEL_SIZE * TILE_SCALING * 2
 PLAYER_Y = SPRITE_PIXEL_SIZE * TILE_SCALING * 1
 
+#Camera tracking player
 RIGHT_FACING = 0
 LEFT_FACING = 1
 
@@ -29,58 +35,112 @@ LAYER_NAME_BACKGROUND = "Background"
 LAYER_NAME_MOVING = "Moving Platforms"
 LAYER_NAME_LADDERS = "Ladders"
 LAYER_NAME_PLAYER = "Player"
+LAYER_NAME_ENEMIES = "Enemies"
 
 def load_texture_pair(filename):
     return [
         arcade.load_texture(filename),
         arcade.load_texture(filename, flipped_horizontally = True),
     ]
+class Entity(arcade.Sprite):
+        def __init__(self, name_folder, name_file):
+            super().__init__()
+            
+            #Default: Make character face to right
+            self.character_face_direction = RIGHT_FACING
 
-class Player(arcade.Sprite):
+            #flipping between image sequences
+            self.cur_texture = 0
+            self.scale =  CHARACTER_SCALING
+            
+            # Track our state
+            self.jumping = False
+            self.climbing = False
+            self.is_on_ladder = False
+
+            #Load Character textures
+            main_path = f":resources:images/animated_characters/{name_folder}/{name_file}"
+
+            #Character standing textures
+            self.idle_texture_pair = load_texture_pair(f"{main_path}_idle.png")
+            self.jump_texture_pair = load_texture_pair(f"{main_path}_jump.png")
+            self.fall_texture_pair = load_texture_pair(f"{main_path}_fall.png")
+
+            #Character walking textures
+            self.walk_textures = []
+            for i in range(8):
+                texture  = load_texture_pair(f"{main_path}_walk{i}.png")
+                self.walk_textures.append(texture)
+            
+            #Character climbing textures
+            self.climbing_textures = []
+            texture = arcade.load_texture(f"{main_path}_climb0.png")
+            self.climbing_textures.append(texture)
+            texture = arcade.load_texture(f"{main_path}_climb1.png")
+            self.climbing_textures.append(texture)
+
+            #Set the initial texture
+            self.texture = self.idle_texture_pair[0]
+
+            self.hit_box = self.texture.hit_box_points
+            
+class Enemy(Entity):
+    def __init__(self, name_folder, name_file):
+        
+        super().__init__(name_folder, name_file)
+
+        self.should_update_walk = 0
+    
+    def update_animation(self, delta_time: float = 1 / 60):
+        
+        if self.change_x < 0 and self.character_face_direction == RIGHT_FACING:
+            self.character_face_direction = LEFT_FACING
+        elif self.change_x > 0 and self.character_face_direction == LEFT_FACING:
+            self.character_face_direction = RIGHT_FACING
+        #idle     
+        if self.change_x == 0:
+            self.texture = self.idle_texture_pair[self.character_face_direction]
+            return
+            
+        #walking animation
+        if self.should_update_walk == 3:
+            self.cur_texture += 1
+            if self.cur_texture > 7:
+                self.cur_texture = 0
+            self.texture = self.walk_textures[self.cur_texture][self.character_face_direction]
+            self.should_update_walk = 0
+            return
+
+        self.should_update_walk += 1
+    
+class RobotEnemy(Enemy):
+    def __init__(self):
+        super().__init__("robot", "robot")
+        
+class ZombieEnemy(Enemy):
+    def __init__(self):
+        super().__init__("zombie", "zombie")
+        
+class Player(Entity):
 
     def __init__(self):
-        super().__init__()
         
-        self.character_face_direction = RIGHT_FACING
+        super().__init__("male_adventurer","maleAdventurer")
 
-        self.cur_texture = 0
-        self.scale =  CHARACTER_SCALING
-        
         self.jumping = False
         self.climbing = False
         self.is_on_ladder = False
 
-        main_path = ":resources:images/animated_characters/male_adventurer/maleAdventurer"
-
-        self.idle_texture_pair = load_texture_pair(f"{main_path}_idle.png")
-        self.jump_texture_pair = load_texture_pair(f"{main_path}_jump.png")
-        self.fall_texture_pair = load_texture_pair(f"{main_path}_fall.png")
-
-        self.walk_textures = []
-        for i in range(8):
-            texture  = load_texture_pair(f"{main_path}_walk{i}.png")
-            self.walk_textures.append(texture)
-        
-        
-        
-        self.climbing_textures = []
-        texture = arcade.load_texture(f"{main_path}_climb0.png")
-        self.climbing_textures.append(texture)
-        texture = arcade.load_texture(f"{main_path}_climb1.png")
-        self.climbing_textures.append(texture)
-
-        self.texture = self.idle_texture_pair[0]
-
-        self.hit_box = self.texture.hit_box_points
-
 
     def update_animation(self, delta_time: float = 1/60):
+        
+        #Flip face left or right
         if self.change_x < 0 and self.character_face_direction == RIGHT_FACING:
             self.character_face_direction = LEFT_FACING
         elif self.change_x > 0 and self.character_face_direction == LEFT_FACING:
             self.character_face_direction = RIGHT_FACING
 
-
+        # Climbing animation
         if self.is_on_ladder:
             self.climbing = True
         if not self.is_on_ladder and self.climbing:
@@ -89,34 +149,40 @@ class Player(arcade.Sprite):
             self.cur_texture += 1
             if self.cur_texture > 7:
                 self.cur_texture = 0
-
         if self.climbing:
             self.texture = self.climbing_textures[self.cur_texture // 4]
             return
             
+        #Jumping animation    
         if self.change_y > 0 and not self.is_on_ladder:
             self.texture = self.jump_texture_pair[self.character_face_direction]
             return
         elif self.change_y < 0 and not self.is_on_ladder:
             self.texture = self.fall_texture_pair[self.character_face_direction]
             return
+
+        #Idle animation            
         if self.change_x == 0:
             self.texture = self.idle_texture_pair[self.character_face_direction]
             return
 
+        #Walking animation
         self.cur_texture += 1
         if self.cur_texture > 7:
             self.cur_texture = 0
-        self.texture = self.walk_textures[self.cur_texture][
-            self.character_face_direction
-        ]
+        self.texture = self.walk_textures[self.cur_texture][self.character_face_direction]
+        
 class Platformer(arcade.Window):  
     def __init__(self):
+        
+        #Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLES)
 
+        #Set the path to start the progoram
         file_path = os.path.dirname(os.path.abspath(__file__))
         os.chdir(file_path)
 
+        # Track current state to see that what key is pressed
         self.left_pressed = False
         self.right_pressed = False
         self.up_pressed = False
@@ -141,21 +207,25 @@ class Platformer(arcade.Window):
         
         self.level = 1
 
+        #Load Sounds
         self.collect_coin_sound = arcade.load_sound(":resources:sounds/coin1.wav") #Coin sound
         self.jump_sound = arcade.load_sound(":resources:sounds/jump1.wav") #Jump Sound
         self.game_over = arcade.load_sound(":resources:sounds/gameover1.wav")
 
+        #Set background color
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
         
     
     def setup(self):
 
+        #Set up the camera
         self.camera = arcade.Camera(self.width, self.height)
-
         self.gui_camera = arcade.Camera(self.width, self.height)
 
+        #Set the map
         map_name = f":resources:tiled_maps/map_with_ladders.json"
         
+        #Layer Specific Options for the map
         layer_options ={
             LAYER_NAME_PLATFORMS : {
                 "use_spatial_hash" : True,
@@ -171,22 +241,60 @@ class Platformer(arcade.Window):
             },
         }
 
+        #Load the TileMap
         self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING, layer_options)
 
+        #Initiate New Scene
         self.scene = arcade.Scene.from_tilemap(self.tile_map)
 
+        #Track Scores
         self.score = 0
         
+        #Set up the player
         self.player_sprite = Player()
         self.player_sprite.center_x = PLAYER_X
         self.player_sprite.center_y = PLAYER_Y
         self.scene.add_sprite(LAYER_NAME_PLAYER, self.player_sprite)
         
+        #Calculate the right edge of map in pixel
         self.end_of_map = self.tile_map.tiled_map.map_size.width * GRID_PIXEL_SIZE
+        
+        enemies_layer = self.tile_map.object_lists[LAYER_NAME_ENEMIES]
 
+        for my_object in enemies_layer:
+            cartesian = self.tile_map.get_cartesian(
+                my_object.shape[0], my_object.shape[1]
+            )
+            print(cartesian)
+            enemy_type = my_object.properties["type"]
+            if enemy_type == "robot":
+                enemy = RobotEnemy()
+            elif  enemy_type == "zombie":
+                enemy = ZombieEnemy()
+            else:
+                raise Exception(f"Unknow enemy type {enemy_type}.")
+            
+            enemy.center_x = math.floor(
+                cartesian[0] * TILE_SCALING * self.tile_map.tile_width
+            )
+            enemy.center_y = math.floor(
+            (cartesian[1] + 0.75)*(self.tile_map.tile_height*TILE_SCALING)
+            )
+            if "boundary_left" in my_object.properties:
+                enemy.boundary_left = my_object.properties["boundary_left"]
+            if "boundary_right" in my_object.properties:
+                enemy.boundary_right = my_object.properties["boundary_right"]
+            if "change_x" in my_object.properties:
+                enemy.change_x  = my_object.properties["change_x"]
+            self.scene.add_sprite(LAYER_NAME_ENEMIES,enemy)
+            
+            
+
+        #Set Background Color
         if self.tile_map.tiled_map.background_color:
             arcade.set_background_color(self.tile_map.tiled_map.background_color)
 
+        #Create the 'physics engine'
         self.physics_engine = arcade.PhysicsEnginePlatformer(
             self.player_sprite,
             [
@@ -199,14 +307,19 @@ class Platformer(arcade.Window):
 
     def on_draw(self):
         
+        #Clear the screen to the background color
         arcade.start_render()
 
+        #Activate game camera
         self.camera.use()
 
+        #Draw our scene
         self.scene.draw()
 
+        #Activate the GUI camera 
         self.gui_camera.use()
 
+        #Draw the score on the screen
         score_text = f"Score: {self.score}"
         arcade.draw_text(
             score_text,
@@ -217,6 +330,8 @@ class Platformer(arcade.Window):
         )
 
     def process_keychange(self):
+        
+        #Process up/down
         if self.up_pressed and not self.down_pressed:
             if self.physics_engine.is_on_ladder():
                 self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
@@ -230,13 +345,15 @@ class Platformer(arcade.Window):
         elif self.down_pressed and not self.up_pressed:
             if self.physics_engine.is_on_ladder():
                 self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
-
+        
+        # Process up/down when on a Ladder and no movement
         if self.physics_engine.is_on_ladder():
             if not self.up_pressed and not self.down_pressed:
                 self.player_sprite.change_y = 0
             elif self.up_pressed and self.down_pressed:
                 self.player_sprite_y = 0
 
+        #Process Left and Right
         if self.right_pressed and not self.left_pressed:
             self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
         elif self.left_pressed and not self.right_pressed:
@@ -245,6 +362,7 @@ class Platformer(arcade.Window):
             self.player_sprite.change_x = 0
 
     def on_key_press(self, key, modifiers):
+        #Called when a key is pressed.
         if key == arcade.key.UP or key == arcade.key.W:
             self.up_pressed = True
         elif key == arcade.key.DOWN or key == arcade.key.S:
@@ -253,9 +371,10 @@ class Platformer(arcade.Window):
             self.left_pressed = True
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.right_pressed = True
-
+        self.process_keychange()
 
     def on_key_release(self, key, modifiers):
+        #Called when a key is release
         if key == arcade.key.UP or key == arcade.key.W:
             self.up_pressed = False
             self.jump_needs_reset = False
@@ -265,6 +384,7 @@ class Platformer(arcade.Window):
             self.left_pressed = False
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.right_pressed = False
+        self.process_keychange()
             
     def center_camera_to_player(self):
         screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
@@ -279,9 +399,12 @@ class Platformer(arcade.Window):
         self.camera.move_to(player_centered, 0.2)
 
     def on_update(self, delta_time):
-        
+        #Movement and game logic
+
+        #Move player with physic engine
         self.physics_engine.update()
 
+        #Update animation
         if self.physics_engine.can_jump():
             self.player_sprite.can_jump = False
         else:
@@ -295,11 +418,35 @@ class Platformer(arcade.Window):
             self.process_keychange()
 
         self.scene.update_animation(
-            delta_time, [LAYER_NAME_COINS, LAYER_NAME_BACKGROUND, LAYER_NAME_PLAYER]
+            delta_time, 
+            [
+                LAYER_NAME_COINS,
+                LAYER_NAME_BACKGROUND, 
+                LAYER_NAME_PLAYER, 
+                LAYER_NAME_ENEMIES,
+                ],
         )
 
-        self.scene.update([LAYER_NAME_MOVING])
+        #Update walls, used with moving platforms
+        self.scene.update([LAYER_NAME_MOVING, LAYER_NAME_ENEMIES])
         
+        #See if the enemy hit a boundary then it reverse
+        for enemy in self.scene.get_sprite_list(LAYER_NAME_ENEMIES):
+            if(
+                enemy.boundary_right
+                and enemy.right > enemy.boundary_right
+                and enemy.change_x > 0
+            ):
+                enemy.change_x *= -1
+                
+            if(
+                enemy.boundary_left
+                and enemy.left < enemy.boundary_left
+                and enemy.change_x < 0
+            ):
+                enemy.change_x *= -1
+                    
+        #See if the moving wall hit a boundary and reverse
         for wall in self.scene.get_sprite_list(LAYER_NAME_MOVING):
             if (
                 wall.boundary_right
@@ -322,28 +469,43 @@ class Platformer(arcade.Window):
             ):
                 wall.change_y *= -1    
         
-        coin_hit_list = arcade.check_for_collision_with_list(
-            self.player_sprite, self.scene.get_sprite_list(LAYER_NAME_COINS)
+        player_collision_list = arcade.check_for_collision_with_lists(
+            self.player_sprite, 
+            [
+                self.scene.get_sprite_list(LAYER_NAME_COINS),
+                self.scene.get_sprite_list(LAYER_NAME_ENEMIES),
+            ],
         )
-        
-        for coin in coin_hit_list:
 
-            if "Points" not in coin.properties:
-                print("Warning, collected a coin without a Points property.")
+        #Loop through each coin character hit and remove coins
+        for collision in player_collision_list:
+
+            #Check how many points this is worth
+            if self.scene.get_sprite_list(LAYER_NAME_ENEMIES) in collision.sprite_lists:
+                arcade.play_sound(self.game_over)
+                self.setup()
+                return
             else:
-                point = int(coin.properties["Points"])
-                self.score += point
 
-            coin.remove_from_sprite_lists()
-            arcade.play_sound(self.collect_coin_sound)
+                if "Points" not in collision.properties: 
+                   print("Warning, collected a coin without a Points property.")
+                else:
+                    points = int(collision.properties["Points"])
+                    self.score += points
 
+                #Remove the coin
+                collision.remove_from_sprite_lists()
+                arcade.play_sound(self.collect_coin_sound)
+
+        #Position the camera
         self.center_camera_to_player()
 
 def main():
+    #Main Function
+    
     window = Platformer()
     window.setup()
     arcade.run()
         
 if __name__ == "__main__":
-    main()        
-
+    main()     
